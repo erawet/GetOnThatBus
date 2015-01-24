164190585c9d29993e2bc9b246c04ab9cc2e696d
Don Wettasinghe <erawet@gmail.com>
2015-01-23 23:55:14 -0500

    Initial Commit

---
+++ GetOnThatBus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetOnThatBus
+//
+//  Created by Don Wettasinghe on 1/23/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
